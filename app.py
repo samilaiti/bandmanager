e320e5b293c99ce56b1a39c85caec1b939f21d1b
Sami Laitinen <sami.p.laitinen@gmail.com>
2024-09-18 22:26:55 +0300

    app initialized

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+from os import getenv
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
