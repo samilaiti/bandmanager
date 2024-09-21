b6475ee9629dc764917f043e9dd1db374272fc15
Sami Laitinen <sami.p.laitinen@gmail.com>
2024-09-21 14:54:24 +0300

    list of bands

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ from sqlalchemy.sql import text
 from os import getenv
 
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    sql = text("SELECT id, name, visible FROM bands ORDER by id ASC")
+    result = db.session.execute(sql)
+    bands = result.fetchall()
+    return render_template("index.html", bands=bands)
