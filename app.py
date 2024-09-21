1a4215e73420116d6aead974b4d278eb9a0f603b
Sami Laitinen <sami.p.laitinen@gmail.com>
2024-09-21 19:05:20 +0300

    Login added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from flask import redirect, render_template, request
+from flask import redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.sql import text
 from os import getenv
 
@@ -15,3 +16,44 @@ def index():
     result = db.session.execute(sql)
     bands = result.fetchall()
     return render_template("index.html", bands=bands)
+
+@app.route("/login",methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    hash_value = generate_password_hash(password)
+    sql = text("SELECT id, password FROM users WHERE username=:username")
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()
+    if not user:
+        return render_template("error.html", message="Virheellinen käyttäjätunnus")
+    else:
+        hash_value = user.password
+        if check_password_hash(hash_value, password):
+            session["username"] = username
+        else:
+            return render_template("error.html", message="Virheellinen salasana")
+
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        hash_value = generate_password_hash(password)
+        sql = text("INSERT INTO users (username, password) VALUES (:username, :password)")
+        db.session.execute(sql, {"username":username, "password":hash_value})
+        db.session.commit()
+        return redirect("/")
+
+    
+
