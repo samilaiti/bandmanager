70ab879a7153934f9a2f5ca8cf089af7683f2305
Sami Laitinen <sami.p.laitinen@gmail.com>
2024-09-25 20:12:03 +0300

    database visible fields added

--- schema.sql
+++ schema.sql
@@ -1,12 +1,14 @@
 CREATE TABLE bands (
     id SERIAL PRIMARY KEY,
-    name TEXT
+    name TEXT,
+    visible BOOLEAN DEFAULT TRUE
 );
 
 CREATE TABLE shows (
     id SERIAL PRIMARY KEY,
     name TEXT,
     date DATE,
+    visible BOOLEAN DEFAULT TRUE,
     venue_id INTEGER REFERENCES venues,
     band_id INTEGER REFERENCES bands
 );
@@ -14,23 +16,27 @@ CREATE TABLE shows (
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username TEXT,
-    password TEXT
+    password TEXT,
+    visible BOOLEAN DEFAULT TRUE
 );
 
 CREATE TABLE venues (
     id SERIAL PRIMARY KEY,
     name TEXT,
     address TEXT,
-    contact TEXT
+    contact TEXT,
+    visible BOOLEAN DEFAULT TRUE
 );
 
 CREATE TABLE songs (
     id SERIAL PRIMARY KEY,
-    name TEXT
+    name TEXT,
+    visible BOOLEAN DEFAULT TRUE
 );
 
 CREATE TABLE setlist (
     id SERIAL PRIMARY KEY,
+    visible BOOLEAN DEFAULT TRUE,
     show_id INTEGER REFERENCES shows,
     song_id INTEGER REFERENCES songs
 );
